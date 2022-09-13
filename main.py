62d09aabda797d0058d4ac8fcfeb5ccca77ddb89
SidLabs <98277470+SidLabs-Online@users.noreply.github.com>
2022-09-13 05:33:13 +0000

    done for future ref

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 import json
 
-response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").text
 
-print(response)
+#print(response)
 
-#response_stacked = json.loads(response)
+response_stacked = json.loads(response)
 
 #print(response_stacked)
 #print(type(response_stacked))
 
-# for keys, values in response_stacked:
-#   print(keys, ":", values)
+print(response_stacked['time'])
+
